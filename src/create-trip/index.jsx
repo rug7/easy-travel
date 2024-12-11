1daadb6261d7368dad34cde54fc20d9e7e146865
Majd Abdo <majdabdo43@gmail.com>
2024-12-11 13:39:19 +0200

    adding google auto complete library

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function CreateTrip() {
   return (
-    <div>createTrip</div>
-  )
+    <div
+      className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-20 " 
+      style={{ marginTop: '80px' }} 
+    >
+      <h2 className="font-bold text-3xl text-white">Tell us your travel preference</h2>
+      <p className="mt-3 text-gray-300 text-xl items-center ">
+        Just provide some basic information, and our trip planner will generate
+        a customized itinerary based on your preferences
+      </p>
+
+      <div className='mt-20'>
+        <div>
+          <h2 className='text-xl my-3 font-medium'> What's your destination of choice?</h2>
+          <GooglePlacesAutocomplete
+          apiKey=''
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CreateTrip
+export default CreateTrip;
