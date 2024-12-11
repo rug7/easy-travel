b9283b475037437c1a08590fdc098680686065dd
Majd Abdo <majdabdo43@gmail.com>
2024-12-11 20:59:45 +0200

    Language Context

---
+++ src/LanguageContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState, useContext } from "react";
+
+const LanguageContext = createContext();
+
+export const useLanguage = () => useContext(LanguageContext);
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState("en"); // Default language is English
+
+  const changeLanguage = (lang) => setLanguage(lang);
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
