7ef6819214634e8da27594dc37223a7769de308a
Majd Abdo <majdabdo43@gmail.com>
2024-12-12 22:56:28 +0200

    header translated

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -5,7 +5,7 @@ import { useLanguage } from "@/context/LanguageContext";
 
 
 function Header() {
-  const { language, changeLanguage } = useLanguage();
+  const { language, changeLanguage , translate } = useLanguage();
 
   return (
     <div className="p-4 shadow-md justify-between flex items-center px-10 bg-white fixed top-0 left-0 right-0 z-50">
@@ -17,7 +17,7 @@ function Header() {
       </div>
       <div className="flex space-x-4 items-center">
         <Button className="bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 hover:text-white hover:scale-105 hover:shadow-lg transition-all">
-          Sign In
+        {translate("signIn")}
         </Button>
         <select
           value={language}
