fce95ca2e7f734006095ac37fcb42beb13f0a50d
Majd Abdo <majdabdo43@gmail.com>
2024-12-14 21:26:29 +0200

    PeopleInput.jsx

---
+++ src/components/custom/PeopleInput.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PeopleInput = ({ options, translate }) => (
+  <div>
+    <h3 className="text-xl font-bold text-white mb-2">
+      {translate("peopleTraveling")}
+    </h3>
+    <div className="grid grid-cols-4 gap-5">
+      {options.map((item) => (
+        <div
+          key={item.id}
+          className="p-4 border rounded-lg hover:shadow-lg bg-gray-700 text-white"
+        >
+          <h2 className="text-4xl">{item.icon}</h2>
+          <h2 className="font-bold mb-2 text-lg">{item.title}</h2>
+          <p className="text-sm text-gray-400">{item.desc}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default PeopleInput;
