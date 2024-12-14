c919603f61ee8eb5dca24a1801ff425023137a67
Majd Abdo <majdabdo43@gmail.com>
2024-12-14 21:27:49 +0200

    Create Trip using components

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import DestinationInput from "@/components/custom/DestinationInput";
+import DaysInput from "@/components/custom/DaysInput";
+import SelectableOptions from "@/components/custom/SelectableOptions";
+import BudgetOptions from "@/components/custom/BudgetOptions";
+import PeopleInput from "@/components/custom/PeopleInput";
 import { useLanguage } from "@/context/LanguageContext";
 import { getTranslatedOptions } from "@/constants/options";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import SelectableOptions from "@/components/custom/SelectableOptions";
-
 
 function CreateTrip() {
+  // State management
   const [place, setPlace] = useState(null);
   const [showMoreQuestions, setShowMoreQuestions] = useState(false);
   const [startDate, setStartDate] = useState(null);
@@ -16,27 +17,16 @@ function CreateTrip() {
   const [numDays, setNumDays] = useState("");
   const { translate, language } = useLanguage();
   const isRTL = language === "he";
-  // Retrieve translated options
+
   const {
     SelectTravelsList,
     SelectBudgetOptions,
     WeatherOptions,
     ActivityOptions,
     SightseeingOptions,
-    AI_PROMPT,
   } = getTranslatedOptions(translate);
 
-
-  const handleToggle = (option) => {
-    if (option === "dates") {
-      setUseDates(true);
-      setNumDays("");
-    } else {
-      setUseDates(false);
-      setStartDate(null);
-      setEndDate(null);
-    }
-  };
+  // Selection states
   const [selectedWeather, setSelectedWeather] = useState([]);
   const [selectedActivities, setSelectedActivities] = useState([]);
   const [selectedSightseeing, setSelectedSightseeing] = useState([]);
@@ -65,173 +55,75 @@ function CreateTrip() {
 
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-20 px-5 mt-20">
-      <h2 className="font-bold text-4xl text-white text-center mb-6" style={{ direction: isRTL ? "rtl" : "ltr" }}>
+      <h2
+        className="font-bold text-4xl text-white text-center mb-6"
+        style={{ direction: isRTL ? "rtl" : "ltr" }}
+      >
         {translate("createTripTitle")}
       </h2>
-      <p className="text-gray-300 text-xl text-center mb-10" style={{ direction: isRTL ? "rtl" : "ltr" }}>
+      <p
+        className="text-gray-300 text-xl text-center mb-10"
+        style={{ direction: isRTL ? "rtl" : "ltr" }}
+      >
         {translate("createTripDescription")}
       </p>
 
-      <div className="bg-gray-800 p-6 rounded-lg shadow-lg space-y-6 flex flex-col gap-9" style={{ direction: isRTL ? "rtl" : "ltr" }}>
+      <div
+        className="bg-gray-800 p-6 rounded-lg shadow-lg space-y-6 flex flex-col gap-9"
+        style={{ direction: isRTL ? "rtl" : "ltr" }}
+      >
         {/* Destination Section */}
-        <div>
-          <h3 className="text-xl font-bold text-white mb-4">
-            {translate("destinationTitle")}
-          </h3>
-          <div className="flex items-center justify-between">
-            <div className="w-4/5"> {/* 80% of the width */}
-              <GooglePlacesAutocomplete
-                apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
-                selectProps={{
-                  value: place,
-                  onChange: (v) => setPlace(v),
-                  placeholder: translate("enterDestination"),
-                }}
-              />
-            </div>
-            <button
-              className="px-4 py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-bold rounded-full shadow-lg hover:scale-105 hover:from-blue-600 hover:to-blue-800 transition-all duration-300"
-              onClick={() => setShowMoreQuestions(!showMoreQuestions)}
-            >
-              {translate("helpMeDecide")}
-            </button>
-          </div>
-        </div>
+        <DestinationInput
+          place={place}
+          setPlace={setPlace}
+          translate={translate}
+          onToggle={() => setShowMoreQuestions(!showMoreQuestions)}
+        />
 
+        {/* Additional Questions */}
         {showMoreQuestions && (
           <div className="space-y-10">
-            <h3 className="text-2xl font-bold text-white text-center">
-              {translate("narrowDown")}
-            </h3>
-
-            {/* Weather */}
             <SelectableOptions
               title={translate("weatherPreference")}
               options={WeatherOptions}
               selectedOptions={selectedWeather}
               onSelect={(id) => handleSelect(id, "weather")}
             />
-
-            {/* Activities */}
             <SelectableOptions
               title={translate("activitiesPreference")}
               options={ActivityOptions}
               selectedOptions={selectedActivities}
               onSelect={(id) => handleSelect(id, "activities")}
-              gridCols="grid-cols-2 md:grid-cols-4" 
+              gridCols="grid-cols-2 md:grid-cols-4"
             />
-
-            {/* Sightseeing */}
             <SelectableOptions
               title={translate("sightseeingPreference")}
               options={SightseeingOptions}
               selectedOptions={selectedSightseeing}
               onSelect={(id) => handleSelect(id, "sightseeing")}
-              gridCols="grid-cols-2 md:grid-cols-4" 
+              gridCols="grid-cols-2 md:grid-cols-4"
             />
-
           </div>
         )}
 
         {/* Number of Days */}
-        <div>
-          <h2 className="text-xl font-bold text-white mb-4">
-            {translate("daysPlanningTitle")}
-          </h2>
-          <div className="bg-gray-800 p-4 rounded-lg shadow-lg">
-            {/* Flex container for inputs */}
-            <div className="flex gap-6">
-              {/* Number of Days Input */}
-              <div className="flex-1">
-                <button
-                  onClick={() => handleToggle("days")}
-                  className={`px-4 py-2 rounded-full text-white font-bold ${!useDates ? "bg-blue-600" : "bg-gray-600"
-                    }`}
-                >
-                  {translate("numberOfDays")}
-                </button>
-                <input
-                  type="number"
-                  value={numDays}
-                  onChange={(e) => setNumDays(e.target.value)}
-                  placeholder={translate("numberOfDays")}
-                  className={`w-full px-4 py-2 bg-gray-700 text-white rounded-full mt-3 ${useDates ? "opacity-50 cursor-not-allowed" : ""
-                    }`}
-                  disabled={useDates}
-                />
-              </div>
+        <DaysInput
+          translate={translate}
+          useDates={useDates}
+          setUseDates={setUseDates}
+          numDays={numDays}
+          setNumDays={setNumDays}
+          startDate={startDate}
+          setStartDate={setStartDate}
+          endDate={endDate}
+          setEndDate={setEndDate}
+        />
 
-              {/* Date Range Picker */}
-              <div className="flex-1">
-                <button
-                  onClick={() => handleToggle("dates")}
-                  className={`px-4 py-2 rounded-full text-white font-bold ${useDates ? "bg-blue-600" : "bg-gray-600"
-                    }`}
-                >
-                  {translate("pickDates")}
-                </button>
-                <div
-                  className={`mt-3 ${!useDates ? "opacity-70 cursor-not-allowed" : ""}`}
-                >
-                  {/* Apply flex-1 class for equal width */}
-
-                  <DatePicker
-                    selected={startDate}
-                    onChange={(update) => {
-                      setStartDate(update[0]);
-                      setEndDate(update[1]);
-                    }}
-                    startDate={startDate}
-                    endDate={endDate}
-                    dateFormat={"dd/MM/yyyy"}
-                    selectsRange
-                    placeholderText={translate("startEndDate")}
-                    className="w-full px-4 py-2 bg-gray-700 text-white rounded-full shadow-md"
-                    isClearable
-                    disabled={!useDates}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         {/* Budget */}
-        <div>
-          <h3 className="text-xl font-bold text-white mb-2">
-            {translate("budgetTitle")}
-          </h3>
-          <div className="grid grid-cols-3 gap-5">
-            {SelectBudgetOptions.map((item) => (
-              <div
-                key={item.id}
-                className="p-4 border rounded-lg hover:shadow-lg bg-gray-700 text-white"
-              >
-                <h2 className="text-4xl">{item.icon}</h2>
-                <h2 className="font-bold mb-2 text-lg">{item.title}</h2>
-                <p className="text-sm text-gray-400">{item.desc}</p>
-              </div>
-            ))}
-          </div>
-        </div>
+        <BudgetOptions options={SelectBudgetOptions} translate={translate} />
 
         {/* Number of People */}
-        <div>
-          <h3 className="text-xl font-bold text-white mb-2">
-            {translate("peopleTraveling")}
-          </h3>
-          <div className="grid grid-cols-4 gap-5">
-            {SelectTravelsList.map((item) => (
-              <div
-                key={item.id}
-                className="p-4 border rounded-lg hover:shadow-lg bg-gray-700 text-white"
-              >
-                <h2 className="text-4xl">{item.icon}</h2>
-                <h2 className="font-bold mb-2 text-lg">{item.title}</h2>
-                <p className="text-sm text-gray-400">{item.desc}</p>
-              </div>
-            ))}
-          </div>
-        </div>
+        <PeopleInput options={SelectTravelsList} translate={translate} />
 
         {/* Generate Trip */}
         <div className="text-center">
