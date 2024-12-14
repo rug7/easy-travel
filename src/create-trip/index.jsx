78495638bf2ae2343997b15946632ba688843cad
Majd Abdo <majdabdo43@gmail.com>
2024-12-14 21:16:03 +0200

    Sellect V with shadow

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -115,14 +115,29 @@ function CreateTrip() {
                 {WeatherOptions.map((option) => (
                   <button
                     key={option.id}
-                    className={`w-full h-48 rounded-xl bg-cover bg-center relative shadow-lg transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-2xl ${selectedWeather.includes(option.id) ? "ring-2 ring-blue-500" : ""
-                      }`}
+                    className="w-full h-48 rounded-xl bg-cover bg-center relative shadow-lg transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-2xl"
                     style={{
                       backgroundImage: `url(${option.image})`,
                     }}
                     onClick={() => handleSelect(option.id, "weather")}
                   >
-                    <div className="absolute inset-0 bg-black/30 rounded-xl"></div>
+                    {/* Checkmark overlay */}
+                    {selectedWeather.includes(option.id) && (
+                      <div className="absolute inset-0 flex items-center justify-center bg-black/40 rounded-xl">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-12 w-12 text-white"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    )}
                     <span className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-center text-white font-semibold text-sm bg-black/50 rounded-full py-1 px-3">
                       {option.title}
                     </span>
@@ -136,18 +151,33 @@ function CreateTrip() {
               <p className="text-lg font-medium text-gray-200 mb-4 text-center">
                 {translate("activitiesPreference")}
               </p>
-              <div className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 {ActivityOptions.map((option) => (
                   <button
                     key={option.id}
-                    className={`w-full h-48 rounded-xl bg-cover bg-center relative shadow-lg transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-2xl ${selectedActivities.includes(option.id) ? "ring-2 ring-blue-500" : ""
-                      }`}
+                    className="w-full h-48 rounded-xl bg-cover bg-center relative shadow-lg transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-2xl"
                     style={{
                       backgroundImage: `url(${option.image})`, // Use images dynamically
                     }}
                     onClick={() => handleSelect(option.id, "activities")}
                   >
-                    <div className="absolute inset-0 bg-black/40 rounded-xl"></div>
+                    {/* Checkmark overlay */}
+                    {selectedActivities.includes(option.id) && (
+                      <div className="absolute inset-0 flex items-center justify-center bg-black/40 rounded-xl">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-12 w-12 text-white"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    )}
                     <span className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-center text-white font-semibold text-sm bg-black/50 rounded-full py-1 px-3">
                       {option.title}
                     </span>
@@ -161,18 +191,33 @@ function CreateTrip() {
               <p className="text-lg font-medium text-gray-200 mb-4 text-center">
                 {translate("sightseeingPreference")}
               </p>
-              <div className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 {SightseeingOptions.map((option) => (
                   <button
                     key={option.id}
-                    className={`w-full h-48 rounded-xl bg-cover bg-center relative shadow-lg transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-2xl ${selectedSightseeing.includes(option.id) ? "ring-2 ring-blue-500" : ""
-                      }`}
+                    className="w-full h-48 rounded-xl bg-cover bg-center relative shadow-lg transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-2xl"
                     style={{
                       backgroundImage: `url(${option.image})`, // Use images dynamically
                     }}
                     onClick={() => handleSelect(option.id, "sightseeing")}
                   >
-                    <div className="absolute inset-0 bg-black/40 rounded-xl"></div>
+                    {/* Checkmark overlay */}
+                    {selectedSightseeing.includes(option.id) && (
+                      <div className="absolute inset-0 flex items-center justify-center bg-black/40 rounded-xl">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-12 w-12 text-white"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    )}
                     <span className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-center text-white font-semibold text-sm bg-black/50 rounded-full py-1 px-3">
                       {option.title}
                     </span>
@@ -180,6 +225,7 @@ function CreateTrip() {
                 ))}
               </div>
             </div>
+
           </div>
         )}
 
