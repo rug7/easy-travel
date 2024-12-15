3ba4d7e5812f3d7b013501d2d3411076e47063b3
Majd Abdo <majdabdo43@gmail.com>
2024-12-15 23:28:33 +0200

    cleared the input of the search destination

--- src/components/custom/DestinationInput.jsx
+++ src/components/custom/DestinationInput.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
-const DestinationInput = ({ place, setPlace, translate, onToggle }) => (
-  <div>
-    <h3 className="text-xl font-bold text-white mb-4">
-      {translate("destinationTitle")}
-    </h3>
-    <div className="flex items-center justify-between">
-      <div className="w-4/5"> {/* 80% of the width */}
-        <GooglePlacesAutocomplete
-          apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
-          selectProps={{
-            value: place,
-            onChange: (v) => setPlace(v),
-            placeholder: translate("enterDestination"),
-          }}
-        />
+const DestinationInput = ({ place, setPlace, translate, onToggle }) => {
+  const handleHelpMeDecide = () => {
+    setPlace(null); // Clear the input field
+    onToggle();     // Call the onToggle function passed as a prop
+  };
+
+  return (
+    <div>
+      <h3 className="text-xl font-bold text-white mb-4">
+        {translate("destinationTitle")}
+      </h3>
+      <div className="flex items-center justify-between">
+        <div className="w-4/5"> {/* 80% of the width */}
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+            selectProps={{
+              value: place,
+              onChange: (v) => setPlace(v),
+              placeholder: translate("enterDestination"),
+            }}
+          />
+        </div>
+        <button
+          className="px-4 py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-bold rounded-full shadow-lg hover:scale-105 hover:from-blue-600 hover:to-blue-800 transition-all duration-300"
+          onClick={handleHelpMeDecide}
+        >
+          {translate("helpMeDecide")}
+        </button>
       </div>
-      <button
-        className="px-4 py-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white font-bold rounded-full shadow-lg hover:scale-105 hover:from-blue-600 hover:to-blue-800 transition-all duration-300"
-        onClick={onToggle}
-      >
-        {translate("helpMeDecide")}
-      </button>
     </div>
-  </div>
-);
+  );
+};
 
 export default DestinationInput;
